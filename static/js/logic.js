1d0269fc88e7e096f84eef194b91c4cfd18d8887
Roshini <roshini.12j@gmail.com>
2020-11-24 23:19:27 -0500

    Adding step2 code fault lines

--- static/js/logic.js
+++ static/js/logic.js
@@ -4,9 +4,9 @@
 var earthquakesURL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 var platesURL = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
 
-// Initialize LayerGroups: earthquakes 
+// Initialize LayerGroups: earthquakes and tectonic plate
 var earthquakes = new L.LayerGroup();
-
+var tectonicPlates = new L.LayerGroup();
 
 // Define Variables for Tile Layers
 
@@ -19,26 +19,45 @@ var grayscaleMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/
     accessToken: API_KEY
   })
   
+  var satelliteMap  = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v9",
+    accessToken: API_KEY
+  })
 
+  var outdoorsMap  = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/outdoors-v11",
+    accessToken: API_KEY
+  }) 
 
 // Define baseMaps object to hold Base Layers
 var baseMaps = {
-    "Grayscale": grayscaleMap
+    "Grayscale": grayscaleMap,
+    "Satellite": satelliteMap,
+    "Outdoors": outdoorsMap
 };
 
 // Create overlay object to hold Overlay Layers
 var overlayMaps = {
-    "Earthquakes": earthquakes
+    "Earthquakes": earthquakes,
+    "Fault Lines": tectonicPlates
 };
 
 // Create Map, Passing In satelliteMap & earthquakes as Default Layers to Display on Load
 var myMap = L.map("mapid", {
     center: [37.09, -95.71],
     zoom: 2,
-    layers: [grayscaleMap, earthquakes]
+    layers: [satelliteMap, earthquakes]
 });
 
-// Create a Layer Control,add the ayer Control to the Map
+// Create a Layer Control,add the layer control to the Map
 L.control.layers(baseMaps, overlayMaps).addTo(myMap);
 
 // Retrieve earthquakesURL (USGS Earthquakes GeoJSON Data) with D3
@@ -98,6 +117,17 @@ d3.json(earthquakesURL, function(earthquakeData) {
     // Add earthquakes Layer to the Map
     earthquakes.addTo(myMap);
 
+    // Retrieve platesURL (Tectonic Plates GeoJSON Data) with D3
+    d3.json(platesURL, function(plateData) {
+        // Create a GeoJSON Layer the plateData
+        L.geoJson(plateData, {
+            color: "#DC143C",
+            weight: 2
+        // Add plateData to tectonicPlates LayerGroups 
+        }).addTo(tectonicPlates);
+        // Add tectonicPlates Layer to the Map
+        tectonicPlates.addTo(myMap);
+    });
       // Set Up Legend
       var legend = L.control({ position: "bottomright" });
       legend.onAdd = function() {
